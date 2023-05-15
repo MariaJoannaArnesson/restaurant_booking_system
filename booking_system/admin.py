5284900de40a804f8c2d61c07387156271fe4aaa
Maria Arnesson <marys.zieminska@gmail.com>
2023-05-15 14:13:45 +0000

    Added properties to models.py and admin.py with class no of guest and reservation

--- booking_system/admin.py
+++ booking_system/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import No_of_guest, Reservation
 
-# Register your models here.
+
+admin.site.register(No_of_guest)
+admin.site.register(Reservation)
--- booking_system/models.py
+++ booking_system/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+
+class No_of_guest(models.Model):
+    guest = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return str(self.guest)
+
+
+class Reservation(models.Model):
+
+    TIME_CHOICES = [
+        ("6 PM", "6 PM"),
+        ("6:30 PM", "6:30 PM"),
+        ("7 PM", "7 PM"),
+        ("7:30 PM", "7:30 PM"),
+        ("8 PM", "8 PM"),
+        ("8:30 PM", "8:30 PM"),
+        ("9 PM", "9 PM"),
+        ("9:30 PM", "9:30 PM"),
+        ("10 PM", "10 PM"),
+        ("10:30 PM", "10:30 PM"),
+    ]
+
+    OCCASSION_CHOICES = [
+        ("Birthday", "Birthday"),
+        ("Anniversary", "Anniversary"),
+        ("Date night ", "Date night "),
+        ("Business Meal", "Business Meal"),
+        ("Other", "Other"),
+        ]
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='restaurant_booking'
+    )
+    no_of_guest = models.ForeignKey(No_of_guest, on_delete=models.CASCADE)
+    date = models.DateField(null=False, blank=False)
+    time = models.CharField(null=True, blank=False, choices=TIME_CHOICES, max_length=60)
+    occassion = models.CharField(max_length=150, choices=OCCASSION_CHOICES, default="Birthday")
+    special_request = models.TextField(max_length=1024, blank=True)
+
+    class Meta:
+        unique_together = ["no_of_guest", "date", "time"]
+
+    def __str__(self):
+        return f'{self.date}'
+
+    
+
+
